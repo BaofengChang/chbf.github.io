005a2797722ed8c963c0e3f5cf5dcd56566c7564
baofeng <baofengchang94@gmail.com>
2024-12-09 15:10:57 +0800

    update a part of information. The page is available now!

---
+++ libs/btn.js
@@ -0,0 +1,139 @@
+// muse文章
+document.getElementById('muse_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/MUSE.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('muse_cite').addEventListener('click', function() {
+    const citeUrl = 'https://ieeexplore.ieee.org/document/9781257'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// dbnetvizor文章
+document.getElementById('dbnetvizor_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/DBNetVizor.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('dbnetvizor_cite').addEventListener('click', function() {
+    const citeUrl = 'https://ieeexplore.ieee.org/document/10587059'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// tzvis文章
+document.getElementById('tzvis_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/TZVis.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('tzvis_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1007/s12650-019-00600-6'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// exevis文章
+document.getElementById('exevis_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/EXE_vis.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('exevis_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1007/s12650-024-00956-4'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// vac2文章
+document.getElementById('vac2_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/vac2.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('vac2_cite').addEventListener('click', function() {
+    const citeUrl = 'https://ieeexplore.ieee.org/document/10758427'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// lander文章
+document.getElementById('lander_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/LANDER.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('lander_cite').addEventListener('click', function() {
+    const citeUrl = 'https://ieeexplore.ieee.org/document/10570041'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// MAVIDSQL文章
+document.getElementById('mavidsql_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/MAVIDSQL.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('mavidsql_cite').addEventListener('click', function() {
+    const citeUrl = 'https://ieeexplore.ieee.org/document/10505215'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// video survey文章
+document.getElementById('video_survey_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/Video-Visualization-survey.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('video_survey_cite').addEventListener('click', function() {
+    const citeUrl = 'https://ieeexplore.ieee.org/document/10584546'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// vivian文章
+document.getElementById('vivian_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/vivian.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('vivian_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1007/s12650-024-00990-2'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// chaoqing文章
+document.getElementById('chaoqing_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/clinical-analysis.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('chaoqing_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1007/s12650-024-00983-1'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// pan文章
+document.getElementById('pan_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/VMExplorer.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('pan_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1631/FITEE.2200547'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// math survey文章
+document.getElementById('math_survey_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/AMOptimization.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('math_survey_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1631/FITEE.2200547'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// wang xizhi文章
+document.getElementById('wangxizhi_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/Tang_Dynasty.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('wangxizhi_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1007/s44267-023-00012-z'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
+
+// wang xizhi文章
+document.getElementById('permvizor_pdf').addEventListener('click', function() {
+    const pdfUrl = 'pdf/PermVizor.pdf'; // 替换为你的PDF文件路径
+    window.open(pdfUrl, '_blank'); // 在新标签页中打开PDF
+});
+document.getElementById('permvizor_cite').addEventListener('click', function() {
+    const citeUrl = 'https://link.springer.com/article/10.1007/s12650-019-00599-w'; // 替换为你的PDF文件路径
+    window.open(citeUrl, '_blank'); // 在新标签页中打开PDF
+});
